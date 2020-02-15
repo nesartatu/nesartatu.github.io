b90fc5859188b3197e0b6086609cebd4567fc969
bitnesar <nesar.bitfountain@gmail.com>
2020-02-15 21:24:38 +0600

    wp website

---
+++ wp-content/themes/spabeauty/templates/template-frontpage.php
@@ -0,0 +1,24 @@
+<?php 
+/**
+Template Name: Home Page
+*/
+		get_header(); 
+		
+		//=========== Get Home Slider ===========//
+		get_template_part('sections/home','slider');
+		
+		//=========== Get Index callout ===========//
+		get_template_part('sections/home', 'calltoaction');	
+		
+		//=========== Get Index Category ===========//
+		get_template_part('sections/home', 'services');	
+		
+		//=========== Get Index Callout ===========//		
+		get_template_part('sections/home', 'feature');		
+		
+		//=========== Get Index News ===========//
+		get_template_part('sections/home', 'blog');
+
+get_footer();
+
+?>
